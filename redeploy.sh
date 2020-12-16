45922fa82b233827077b208b850adceeed82054b
Jose Alonso <jose@alonsomoya.com>
2020-12-16 21:20:41 +0100

    new github username

--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --no-cache -t josealo/hola:latest --push .
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 --no-cache -t josealo/hola:latest --push . && \
 kubectl rollout restart deployment.apps/hola
